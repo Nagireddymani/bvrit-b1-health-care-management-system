0d25e5162cb03dae905c28a8df6df0a7b11de696
Ilovedad1@# <Nagireddymani>
2020-05-01 12:01:28 +0530

    Added Eureka

---
+++ hcms-eureka-server/src/test/java/com/capg/hcms/eurekaserver/HcmsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.hcms.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcmsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
