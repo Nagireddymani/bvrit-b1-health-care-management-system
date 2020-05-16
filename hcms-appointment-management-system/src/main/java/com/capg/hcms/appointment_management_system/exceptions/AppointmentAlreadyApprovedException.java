64eb88095e96d17d5d8b833e4753720fe7d575cf
boolarohan <61136368+boolarohan@users.noreply.github.com>
2020-05-16 20:01:47 +0530

    Update AppointmentAlreadyApprovedException.java

--- hcms-appointment-management-system/src/main/java/com/capg/hcms/appointment_management_system/exceptions/AppointmentAlreadyApprovedException.java
+++ hcms-appointment-management-system/src/main/java/com/capg/hcms/appointment_management_system/exceptions/AppointmentAlreadyApprovedException.java
@@ -3,7 +3,7 @@ package com.capg.hcms.appointment_management_system.exceptions;
 /*******************************************************************************************************************************
 -Author                   :     Boola Rohan
 -Created/Modified Date    :     27-04-2020
--Description              :     
+-Description              :     AppointmentAlreadyApprovedException class to handle runtime exception
 *******************************************************************************************************************************/
 
 public class AppointmentAlreadyApprovedException extends RuntimeException {
