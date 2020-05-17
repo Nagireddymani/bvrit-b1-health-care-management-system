f2c0b143b08d5e39a2200ee5c80d6bcd1795d65a
boolarohan <61136368+boolarohan@users.noreply.github.com>
2020-05-18 00:50:36 +0530

    Update HcmsApproveAppointmentMsApplicationTestsTest.java

--- hcms-appointment-management-system/src/test/java/com/capg/hcms/appointmentms/HcmsApproveAppointmentMsApplicationTestsTest.java
+++ hcms-appointment-management-system/src/test/java/com/capg/hcms/appointmentms/HcmsApproveAppointmentMsApplicationTestsTest.java
@@ -1,19 +1,19 @@
 package com.capg.hcms.appointmentms;
 
-//import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-//import java.math.BigInteger;
+import java.math.BigInteger;
 
-//import org.hibernate.LazyInitializationException;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.LazyInitializationException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.capg.hcms.appointment_management_system.HcmsAppointmentMsApplication;
-//import com.capg.hcms.appointment_management_system.controller.ApproveAppointmentController;
-//import com.capg.hcms.appointment_management_system.repository.IAppointmentMSRepo;
-//import com.capg.hcms.appointment_management_system.service.AppointmentMSServiceImp;
+import com.capg.hcms.appointment_management_system.controller.ApproveAppointmentController;
+import com.capg.hcms.appointment_management_system.repository.IAppointmentMSRepo;
+import com.capg.hcms.appointment_management_system.service.AppointmentMSServiceImp;
 
 /*******************************************************************************************************************************
 -Author                   :     Boola Rohan
@@ -26,28 +26,28 @@ import com.capg.hcms.appointment_management_system.HcmsAppointmentMsApplication;
 @SpringBootTest(classes = HcmsAppointmentMsApplication.class)
 class HcmsApproveAppointmentMsApplicationTestsTest {
 
-	//@Autowired
-	//AppointmentMSServiceImp service;
-	//@Autowired
-	//IAppointmentMSRepo repo;
-	//@Autowired
-	//ApproveAppointmentController approveAppointment;
+	@Autowired
+	AppointmentMSServiceImp service;
+	@Autowired
+	IAppointmentMSRepo repo;
+	@Autowired
+	ApproveAppointmentController approveAppointment;
 
-	//@Test
-	//void testGetAllAppointments() {
-		//assertEquals(true, !repo.findAll().isEmpty());
-	//}
+	@Test
+	void testGetAllAppointments() {
+		assertEquals(true, !repo.findAll().isEmpty());
+	}
 
 	
 
-	//@Test
-	//void testApproveAppointmentException() {
+	@Test
+	void testApproveAppointmentException() {
 
-		//Assertions.assertThrows(LazyInitializationException.class, () -> {
+		Assertions.assertThrows(LazyInitializationException.class, () -> {
 
-			//approveAppointment.approveAppoinment(BigInteger.valueOf(1), true);
-		//});
+			approveAppointment.approveAppoinment(BigInteger.valueOf(1), true);
+		});
 
-	//}
+	}
 
 }
