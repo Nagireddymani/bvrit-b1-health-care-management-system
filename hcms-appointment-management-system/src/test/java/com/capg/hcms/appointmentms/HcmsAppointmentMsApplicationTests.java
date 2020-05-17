53ff6177ac7957fe954db1230e0774fcf1fe9e58
boolarohan <61136368+boolarohan@users.noreply.github.com>
2020-05-18 00:48:56 +0530

    Update HcmsAppointmentMsApplicationTests.java

--- hcms-appointment-management-system/src/test/java/com/capg/hcms/appointmentms/HcmsAppointmentMsApplicationTests.java
+++ hcms-appointment-management-system/src/test/java/com/capg/hcms/appointmentms/HcmsAppointmentMsApplicationTests.java
@@ -1,44 +1,44 @@
 package com.capg.hcms.appointmentms;
 
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-//import java.math.BigInteger;
-//import java.time.LocalDateTime;
+import java.math.BigInteger;
+import java.time.LocalDateTime;
 
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 
 import com.capg.hcms.appointment_management_system.HcmsAppointmentMsApplication;
-//import com.capg.hcms.appointment_management_system.exceptions.AppointmentNotFoundException;
-//import com.capg.hcms.appointment_management_system.exceptions.SlotNotAvailableException;
-//import com.capg.hcms.appointment_management_system.model.Appointment;
-//import com.capg.hcms.appointment_management_system.service.AppointmentMSServiceImp;
-//import com.capg.hcms.appointment_management_system.service.IAppointmentMSService;
+import com.capg.hcms.appointment_management_system.exceptions.AppointmentNotFoundException;
+import com.capg.hcms.appointment_management_system.exceptions.SlotNotAvailableException;
+import com.capg.hcms.appointment_management_system.model.Appointment;
+import com.capg.hcms.appointment_management_system.service.AppointmentMSServiceImp;
+import com.capg.hcms.appointment_management_system.service.IAppointmentMSService;
 
 @SpringBootTest(classes = HcmsAppointmentMsApplication.class)
 class HcmsAppointmentMsApplicationTests {
 
-	//@Autowired
-	//IAppointmentMSService service;
+	@Autowired
+	IAppointmentMSService service;
 	
-	//Appointment appointment;
-	//Appointment appointment1;
-	//Appointment appointment2;
+	Appointment appointment;
+	Appointment appointment1;
+	Appointment appointment2;
 	
 
 	
-	//@BeforeEach
-	//public void setUp() {
+	@BeforeEach
+	public void setUp() {
 
-		//appointment = new Appointment("120", BigInteger.valueOf(0), "23", LocalDateTime.of(2020, 6, 20, 9, 30), false);
-		//appointment1=new Appointment("230", BigInteger.valueOf(0), "12", LocalDateTime.of(2020, 5, 9, 9, 30), false);
-		//appointment2=new Appointment("230", BigInteger.valueOf(0) , "12", LocalDateTime.of(2020,9 , 9, 11, 30), false);
-	//}
+		appointment = new Appointment("120", BigInteger.valueOf(0), "23", LocalDateTime.of(2020, 6, 20, 9, 30), false);
+		appointment1=new Appointment("230", BigInteger.valueOf(0), "12", LocalDateTime.of(2020, 5, 9, 9, 30), false);
+		appointment2=new Appointment("230", BigInteger.valueOf(0) , "12", LocalDateTime.of(2020,9 , 9, 11, 30), false);
+	}
 
 	//@Test
 	//public void testMakeAppointment() {
@@ -49,21 +49,21 @@ class HcmsAppointmentMsApplicationTests {
 
 	//}
 
-	//@Test
-	//public void testSlotNotAvailableException() {
+	@Test
+	public void testSlotNotAvailableException() {
 
-		//assertThrows(SlotNotAvailableException.class, () -> {service.makeAppointment(appointment);});
-		//assertThrows(SlotNotAvailableException.class, ()->{service.makeAppointment(appointment1);});
-		//assertThrows(SlotNotAvailableException.class, ()->{service.makeAppointment(appointment2);});
+		assertThrows(SlotNotAvailableException.class, () -> {service.makeAppointment(appointment);});
+		assertThrows(SlotNotAvailableException.class, ()->{service.makeAppointment(appointment1);});
+		assertThrows(SlotNotAvailableException.class, ()->{service.makeAppointment(appointment2);});
 
-	//}
+	}
 	
 
-	//@Test
-	//public void testAppointmentNotFoundException() {
-		//assertThrows(AppointmentNotFoundException.class, () -> {
-			//service.getAppointment(BigInteger.valueOf(12));
-		//});
-	//}
+	@Test
+	public void testAppointmentNotFoundException() {
+		assertThrows(AppointmentNotFoundException.class, () -> {
+			service.getAppointment(BigInteger.valueOf(12));
+		});
+	}
 
 }
